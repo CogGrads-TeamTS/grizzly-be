7bf1fb7dc6e03e903e12f5fecd994f6f6633a814
Danyon Edwards <matchstick_dan@hotmail.com>
2018-05-30 14:50:06 +1000

    Changed the auto increment of id in categories

--- src/main/java/com/ts/grizzlybe/Model/Category.java
+++ src/main/java/com/ts/grizzlybe/Model/Category.java
@@ -7,7 +7,7 @@ import javax.persistence.*;
 @Entity
 public class Category {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
     
     private String name;
