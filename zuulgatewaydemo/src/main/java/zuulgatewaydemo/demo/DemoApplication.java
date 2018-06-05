4a619533c2c8b438ea68faa7b4aec43bdd92ae86
Blake Howe <blakehowe@hotmail.com>
2018-06-05 21:55:39 +1000

    gateway adjustments

--- zuulgatewaydemo/src/main/java/zuulgatewaydemo/demo/DemoApplication.java
+++ zuulgatewaydemo/src/main/java/zuulgatewaydemo/demo/DemoApplication.java
@@ -2,11 +2,11 @@ package zuulgatewaydemo.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableZuulServer
+@EnableZuulProxy
 @EnableFeignClients
 public class DemoApplication {
 
