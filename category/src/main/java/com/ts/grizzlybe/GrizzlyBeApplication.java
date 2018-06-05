03bc8adb6b2518b63e1911ec80a82b3886ceb15b
Blake Howe <blakehowe@hotmail.com>
2018-06-05 15:20:54 +1000

    fixed stupid string

--- category/src/main/java/com/ts/grizzlybe/GrizzlyBeApplication.java
+++ category/src/main/java/com/ts/grizzlybe/GrizzlyBeApplication.java
@@ -2,9 +2,7 @@ package com.ts.grizzlybe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
